c0dea5ee67a956ef6cc06f2cd87ba81b5b488679
Gokul-13579 <gokulcandy111@gmail.com>
2025-07-13 00:13:51 +0530

    Add files via upload

---
+++ day1/LearnStrings.java
@@ -0,0 +1,23 @@
+package week3.day1;
+public class LearnStrings {
+	public static void main(String[] args) {
+		String companyName="TestLeaf";		
+		char[] characters = companyName.toCharArray();
+		for (int i = characters.length-1; i>=0;i--) {
+			System.out.println(characters[i]);
+		}
+	}
+	
+}
+		
+		
+			
+		
+		
+	    
+		
+		
+				
+		
+	
+
